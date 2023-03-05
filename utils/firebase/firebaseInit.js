6b40ce8fdbdc77d2ebff440a425eb3df095710cc
kuo <charliech17@yahoo.com.tw>
2023-03-05 20:25:24 +0800

    修正env檔參數

--- utils/firebase/firebaseInit.js
+++ utils/firebase/firebaseInit.js
@@ -5,15 +5,14 @@ export function firebaseInit() {
     const config = useRuntimeConfig()
     // Your web app's Firebase configuration
     const firebaseConfig = {
-        apiKey: config.FIREBASE_API_KEY, 
-        authDomain: config.FIREBASE_AUTH_DOMAIN, 
-        databaseURL: config.FIREBASE_DATABASE_URL, 
-        projectId: config.FIREBASE_PROJECT_ID, 
-        storageBucket: config.FIREBASE_STORAGE_BUCKET, 
-        messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID, 
-        appId: config.FIREBASE_APP_ID, 
+        apiKey: config.public.FIREBASE_API_KEY, 
+        authDomain: config.public.FIREBASE_AUTH_DOMAIN, 
+        databaseURL: config.public.FIREBASE_DATABASE_URL, 
+        projectId: config.public.FIREBASE_PROJECT_ID, 
+        storageBucket: config.public.FIREBASE_STORAGE_BUCKET, 
+        messagingSenderId: config.public.FIREBASE_MESSAGING_SENDER_ID, 
+        appId: config.public.FIREBASE_APP_ID, 
     };
-    
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
     return app
