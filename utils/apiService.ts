76231859abcee2945f237b334648a01949b81519
kuo <charliech17@yahoo.com.tw>
2023-03-12 23:17:32 +0800

    新增 apiService有回傳參數時的處理

--- utils/apiService.ts
+++ utils/apiService.ts
@@ -1,5 +1,10 @@
 export const apiService = async (apiFunction: Function) => {
     toggleShowLoading(true)
-    await apiFunction().catch(()=>toggleShowLoading(false))
+    const returnValue = await apiFunction().catch(()=>{
+        // TODO 將所有error的處理在這邊處理
+        toggleShowLoading(false)
+        return
+    })
     toggleShowLoading(false)
+    return returnValue
 }
