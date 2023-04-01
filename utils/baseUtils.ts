0e52c5fda8e7b20f79811d45f6de4e1e5a597cc7
Josh <kcchang1994@gmail.com>
2023-04-01 22:26:36 +0800

    列出裝置所有輸入源（audio 或 video input）

--- utils/baseUtils.ts
+++ utils/baseUtils.ts
@@ -30,6 +30,25 @@ export const toggleStreamOutput = (stream: undefined | MediaStream, inputType: '
   }
 
 
+export const enumerateAllSource = () => {
+    if (!navigator.mediaDevices?.enumerateDevices) {
+        console.log("enumerateDevices() not supported.");
+    } else {
+    // List cameras and microphones.
+    navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+        devices.forEach((device) => {
+            console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+        });
+        })
+        .catch((err) => {
+        console.error(`${err.name}: ${err.message}`);
+        });
+    }
+}
+
+
 // ############## type  ############## //
 
 interface mediaConstraintsType {
