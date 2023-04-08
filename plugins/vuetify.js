8bb61047202531dd766c20679cc1f120444898f7
Josh <kcchang1994@gmail.com>
2023-04-08 23:57:58 +0800

    刪除全部vuetify import 的程式碼

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,13 +1,9 @@
 // plugins/vuetify.js
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: false,
-    components,
-    directives,
   })
 
   nuxtApp.vueApp.use(vuetify)
