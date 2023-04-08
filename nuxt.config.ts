de11a8ed40656fd3d5d28fd38bf9ff26cb1c7c73
Josh <kcchang1994@gmail.com>
2023-04-08 23:39:05 +0800

    增加vite tree-shake 加快vuetify載入速度

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
         '@pinia/nuxt',
         '@vueuse/nuxt',
+        async (options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+              config?.plugins?.push(vuetify());
+           });
+        }
     ],
     css: [
         'vuetify/lib/styles/main.sass',
