3bd9fbe982b43e9836f5eb0d24358d439394cc25
kuo <charliech17@yahoo.com.tw>
2023-04-15 10:34:28 +0800

    加上haderIconUtils

---
+++ utils/icons/HeaderIconUtils.ts
@@ -0,0 +1,19 @@
+import mdiAccountCircle from '~icons/mdi/account-circle'
+import mdiHomeCity from '~icons/mdi/home-city'
+import mdiAccount from '~icons/mdi/account'
+import mdiLogout from '~icons/mdi/logout'
+import mdiMenu from '~icons/mdi/menu'
+import mdiAccountGroup from '~icons/mdi/account-group'
+import mdiBookEducationOutline  from '~icons/mdi/book-education-outline'
+import mdiCurrencyUsd  from '~icons/mdi/currency-usd'
+
+export const headerMdiIcons = {
+    mdiAccountCircle,
+    mdiHomeCity,
+    mdiAccount,
+    mdiLogout,
+    mdiMenu,
+    mdiAccountGroup,
+    mdiBookEducationOutline,
+    mdiCurrencyUsd,
+}
