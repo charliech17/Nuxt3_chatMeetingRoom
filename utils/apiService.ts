4da3f57eac2f5cb51b5ee22eb83330368917851c
kuo <charliech17@yahoo.com.tw>
2023-04-29 08:57:40 +0800

    api loading錯誤發生的alert

--- utils/apiService.ts
+++ utils/apiService.ts
@@ -1,6 +1,7 @@
 export const apiService = async (apiFunction: Function) => {
     toggleShowLoading(true)
-    const returnValue = await apiFunction().catch(()=>{
+    const returnValue = await apiFunction().catch((error: Error)=>{
+        alert('發生錯誤，程式可能無法正常執行，建議重新整理。錯誤訊息: '+ error)
         // TODO 將所有error的處理在這邊處理
         toggleShowLoading(false)
         return
