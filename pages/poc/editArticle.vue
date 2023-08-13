09d4d4dc633c887affd48ce8c5c8c00e670449cd
kuo <charliech17@yahoo.com.tw>
2023-08-13 18:58:05 +0800

    Refactor - 改變背景樣式為style寫法

--- pages/poc/editArticle.vue
+++ pages/poc/editArticle.vue
@@ -4,7 +4,7 @@
             <div>
                 <div>背景色</div>
                 <v-btn
-                    @click="highLight"
+                    @click="() => addStyle('background-color','rgb(241, 198, 57)')"
                     :icon="editArticleIcons.mdiSelectColor"
                 />
             </div>
@@ -18,16 +18,16 @@ import {editArticleIcons} from "@/utils/icons/editArticle"
 
 const paragraphRef = ref<HTMLDivElement | null>(null)
 
-function highLight() {
+function addStyle(styleName: string, styleValue: string) {
     // 沒有選取內容
     if(window.getSelection()?.toString() === "") return 
     
     // 已有highLight文字 => 取消highlight
-    const isDoRemove = judgeRemoveHighlight()
+    const isDoRemove = judgeRemoveStyle(styleName)
     if(isDoRemove) return
     
     // 未有highLight文字 => 將文字highlight
-    doHighlight()
+    doHighlight(styleName,styleValue)
 }
 
 function addNewParagraph() {
@@ -43,38 +43,45 @@ function addNewParagraph() {
     const newParagraph = document.createElement('div')
     newParagraph.classList.add("edit_text_paragraph")
     newParagraph.contentEditable = "true"
-    newParagraph.innerHTML = `你好<span class="edit_text_highlight">哇哇</span>阿阿<span class="edit_text_highlight">我哇</span>哇我很好耶`
+    newParagraph.innerHTML = `你好<span style="background-color:blue;margin:4px">哇哇</span>阿阿<span style="background-color: rgb(241, 198, 57);">我哇</span>哇我很好耶`
     newParagraph.addEventListener("click",(event)=> event.stopPropagation())
     paragraphRef.value.appendChild(newParagraph)
 }
 
-function judgeRemoveHighlight() {
+function judgeRemoveStyle(styleName: string) {
     const selection = document.getSelection() as Selection
     const {focusNode,anchorNode,focusOffset,anchorOffset} = selection
     if(focusNode === anchorNode) {
         const backWard = anchorOffset > focusOffset
         const juggeNode = backWard ? anchorNode : focusNode
-        const parentElement = juggeNode?.parentElement
-        const grandpa = parentElement?.parentElement
+        const parentElement = juggeNode?.parentElement as HTMLElement
+        const grandpa = parentElement.parentElement
+        const elementStyles = parentElement.style.cssText
 
-        if(parentElement?.classList.contains("edit_text_highlight")) {
+        if(elementStyles.includes(styleName)) {
             const startPos = backWard ? focusOffset : anchorOffset 
             const endPos = backWard ?  anchorOffset : focusOffset
             // @ts-ignore
             const textLength = juggeNode.length
+
             let newHTML = ''
         
             if(startPos !== 0) {
                 const highlight1 = juggeNode?.textContent?.slice(0,startPos)
-                newHTML += `<span class="edit_text_highlight">${highlight1}</span>`
+                newHTML += `<span style="${elementStyles}">${highlight1}</span>`
             }    
 
+            const otherCss = getOtherCssText(parentElement,styleName)
             const unHighlight = juggeNode?.textContent?.slice(startPos,endPos)
-            newHTML += unHighlight
+            newHTML += (otherCss 
+                        ? `<span style="${otherCss}">${unHighlight}</span>`
+                        : unHighlight
+                    )
+            console.log("otherCss",otherCss)
 
             if(endPos !== textLength) {
                 const highlight2 = juggeNode?.textContent?.slice(endPos,textLength)
-                newHTML += `<span class="edit_text_highlight">${highlight2}</span>`
+                newHTML += `<span style="${elementStyles}">${highlight2}</span>`
             }
 
             parentElement.outerHTML = newHTML
@@ -86,43 +93,47 @@ function judgeRemoveHighlight() {
     }
 }
 
-function doHighlight() {
-    doStartEndNoHighlight()
-    doStartOrEndHighlight()
+function doHighlight(styleName: string,styleValue:string) {
+    doStartEndNoHighlight(styleName,styleValue)
+    doStartOrEndHighlight(styleName,styleValue)
 }
 
-function doStartEndNoHighlight() {
-    const {anchorNode, focusNode} = document.getSelection() as Selection
+function doStartEndNoHighlight(styleName: string,styleValue:string) {
+    const {anchorNode, focusNode}: {anchorNode: Selection['anchorNode'],focusNode: Selection['focusNode']} = document.getSelection() as Selection
 
     if( 
-        !anchorNode?.parentElement?.classList.contains("edit_text_highlight") 
-        && !focusNode?.parentElement?.classList.contains("edit_text_highlight")
+        !anchorNode?.parentElement?.style.cssText.includes(styleName)
+        && !focusNode?.parentElement?.style.cssText.includes(styleName)
     ) {
         const surrElement = document.createElement("span")
         const range = window.getSelection()?.getRangeAt(0)
-        surrElement.classList.add("edit_text_highlight")
+        surrElement.style.setProperty(styleName,styleValue)
         range?.surroundContents(surrElement)
 
         surrElement.childNodes.forEach((node) => {
             // @ts-ignore
-            if(node.classList && node.classList.contains("edit_text_highlight")) {
-                const nodeText = node.textContent as string
+            if(node.style && node.style.cssText.includes(styleName)) {
+                 // @ts-ignore
+                const otherCss = getOtherCssText(node,styleName)
+                const nodeText = otherCss 
+                                ? `<span style="${otherCss}">${node.textContent}</span>`
+                                : node.textContent as string
                 node.replaceWith(nodeText)
             }
         })
         surrElement.normalize()
 
-        removeConcatElement(surrElement)
+        removeConcatElement(surrElement,styleName,styleValue)
     }
 }
 
-function doStartOrEndHighlight() {
+function doStartOrEndHighlight(styleName: string,styleValue:string) {
     const {anchorNode, anchorOffset, focusNode, focusOffset} = document.getSelection() as Selection
     const backWard = isBackWard()
     const anchorParentElement = anchorNode?.parentElement
     const focusParentElement = focusNode?.parentElement
-    const isAnchorContains = anchorParentElement?.classList.contains("edit_text_highlight")
-    const isFocusContains = focusParentElement?.classList.contains("edit_text_highlight")
+    const isAnchorContains = anchorParentElement?.style.cssText.includes(styleName)
+    const isFocusContains = focusParentElement?.style.cssText.includes(styleName)
     if(isAnchorContains || isFocusContains) {
         const finalAnchorOffset = backWard 
                                 ? isAnchorContains ? anchorNode?.textContent?.length : anchorOffset
@@ -135,7 +146,7 @@ function doStartOrEndHighlight() {
         
         const surrElement = document.createElement("span")
         const range = window.getSelection()?.getRangeAt(0) as Range
-        surrElement.classList.add("edit_text_highlight")
+        surrElement.style.setProperty(styleName,styleValue)
 
         surrElement.appendChild(range.extractContents())
         range.insertNode(surrElement)
@@ -143,17 +154,21 @@ function doStartOrEndHighlight() {
         surrElement.childNodes.forEach((node)=> {
             if(node.nodeName === "#text") return
             // @ts-ignore
-            if(node.classList.contains("edit_text_highlight")) {
-                node.replaceWith(node.textContent as string)
+            if(node.style && node.style.cssText.includes(styleName)) {
+                // @ts-ignore
+                const otherCss = getOtherCssText(node,styleName)
+                node.replaceWith(otherCss 
+                                ? `<span style="${otherCss}">${node.textContent}</span>`
+                                : node.textContent as string)
             }
         })
 
         surrElement.normalize()
-        removeConcatElement(surrElement)
+        removeConcatElement(surrElement,styleName,styleValue)
     }
 }
 
-function removeConcatElement(surrElement: any) {
+function removeConcatElement(surrElement: any, styleName: string, styleValue: string) {
     let parentEl = surrElement.parentElement
     while(parentEl?.parentElement) {
         if(parentEl?.classList.contains("edit_text_paragraph")) {
@@ -168,13 +183,21 @@ function removeConcatElement(surrElement: any) {
         // @ts-ignore
         const nodeHTML = node.outerHTML ? node.outerHTML : nodeText
         if(!nodeText) return
-        // @ts-ignore
-        if(node.classList && node.classList.contains("edit_text_highlight")) {
+        // // @ts-ignore
+        if( node.style 
+            && node.style.cssText.includes(styleName) 
+            && node.style.getPropertyValue(styleName) === styleValue
+        ) {
             console.log("!!!",node)
-            tempHTML += nodeText
+            const otherCss = getOtherCssText(node,styleName)
+            tempHTML += otherCss 
+                        ? `<span style="${otherCss}">${nodeText}</span>`
+                        : nodeText
+            console.log(tempHTML)
         } else if(tempHTML) {
             // @ts-ignore
-            finalHTML += `<span class="edit_text_highlight">${tempHTML}</span>` + `${nodeHTML}`
+            finalHTML += (`<span style="${styleName}:${styleValue};">${tempHTML}</span>` 
+                            + `${nodeHTML}`)
             tempHTML = ''
         } else {
             // @ts-ignore
@@ -183,7 +206,7 @@ function removeConcatElement(surrElement: any) {
     })
 
     if(!parentEl) return
-    parentEl.innerHTML = finalHTML + (tempHTML ? `<span class="edit_text_highlight">${tempHTML}</span>` : '')
+    parentEl.innerHTML = finalHTML + (tempHTML ? `<span style="${styleName}:${styleValue};">${tempHTML}</span>` : '')
 }
 
 function isBackWard() {
@@ -198,6 +221,11 @@ function isBackWard() {
         }
         
     return backward
+}
+
+function getOtherCssText(node: HTMLElement,styleName:string) {
+    const cssPros = node.style.removeProperty(styleName)
+    return node.style.cssText
 }
 
 </script>
