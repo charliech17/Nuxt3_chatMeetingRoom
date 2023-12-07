0572d067e3f5fcedb72e8ac960e8d99686f4aa19
kuo <charliech17@yahoo.com.tw>
2023-12-08 00:52:15 +0800

    新增手機持續滑動(momentum scroll)偵測，重構程式(重複邏輯抽成公用function)

--- utils/index/leftScroll.ts
+++ utils/index/leftScroll.ts
@@ -6,13 +6,31 @@ let teact_part: null| HTMLElement;
 let isInTeach = false
 let headerHeightMargin = 0;
 
+function removeScrollEvtFuc() {
+    scrollSection!.removeEventListener("scroll",handleScroll)
+}
+
 function handleScroll() {
+    judgeScrollInside(removeScrollEvtFuc)
+}
+
+function judgeScrollInside(removeEvtFuc: Function) {
     if(teact_part!.getBoundingClientRect().top <= headerHeightMargin && teact_part!.getBoundingClientRect().bottom > 0) {
         const scrollSection = document.getElementById('mainContent_scrollSection_ID')
         scrollSection!.style.overflow = "hidden"
         teact_part!.style.overflowX = "auto"
         isInTeach = true
-        scrollSection!.removeEventListener("scroll",handleScroll)
+        removeEvtFuc()
+        return true
+    }
+    return false
+}
+
+function removeWheelEvtFuc(isMoveForward: boolean) {
+    if(isMoveForward) {
+        window.removeEventListener("wheel",handleWheel)
+    } else {
+        scrollSection!.addEventListener("scroll",handleScroll)
     }
 }
 
@@ -21,74 +39,65 @@ function handleWheel(evt: WheelEvent) {
     if(!teact_part) return
 
     if(isInTeach && evt.deltaY > 0) {
+        judgeMoveDirection(true,removeWheelEvtFuc,scollSpeed)
+    } else if(isInTeach && evt.deltaY < 0) {
+        judgeMoveDirection(false,removeWheelEvtFuc,scollSpeed)
+    }
+}
+
+function judgeMoveDirection(isMoveForward: boolean,handleFunc: Function, scollSpeed: number) {
+    if(isMoveForward) {
         teact_part!.scrollLeft = teact_part!.scrollLeft + scollSpeed
-        console.log(teact_part.scrollLeft,teact_part.scrollWidth - teact_part.clientWidth)
-        if(teact_part.scrollLeft >= (teact_part.scrollWidth - teact_part.clientWidth - 3) ) {
-            teact_part.scrollLeft = teact_part.scrollWidth - teact_part.clientWidth
-            const newScroll = scrollSection!.scrollTop + teact_part.getBoundingClientRect().bottom
+        if(teact_part!.scrollLeft >= (teact_part!.scrollWidth - teact_part!.clientWidth - 3) ) {
+            const newScroll = scrollSection!.scrollTop + teact_part!.getBoundingClientRect().bottom
             scrollSection!.style.overflow = ""
             smoothScroll(newScroll,"mainContent_scrollSection_ID",1)
             isInTeach = false
-            teact_part.style.overflowX = "hidden"
-            teact_part.scrollLeft = 0
-            window.removeEventListener("wheel",handleWheel)
+            teact_part!.style.overflowX = "hidden"
+            teact_part!.scrollLeft = 0
+            handleFunc(isMoveForward)
         }
-    } else if(isInTeach && evt.deltaY < 0) {
-        teact_part.scrollLeft = teact_part.scrollLeft - scollSpeed
-        if(teact_part.scrollLeft <= 0) {
-            teact_part.style.overflowX = "hidden"
-            teact_part.scrollLeft = 0
-            
-            const newScroll = scrollSection!.scrollTop - teact_part.getBoundingClientRect().top
+    } else {
+        teact_part!.scrollLeft = teact_part!.scrollLeft - scollSpeed
+        if(teact_part!.scrollLeft <= 0) {
+            teact_part!.style.overflowX = "hidden"
+            teact_part!.scrollLeft = 0
+            const newScroll = scrollSection!.scrollTop - teact_part!.getBoundingClientRect().top
             smoothScroll(newScroll,"mainContent_scrollSection_ID",2)
             scrollSection!.style.overflow = ""
             isInTeach = false
-            scrollSection!.addEventListener("scroll",handleScroll)
+            handleFunc(isMoveForward)
         }
     }
 }
 
 let txs=0,txe=0,tys=0,tye=0;
 
+function detectMobileScroll() {
+    function removeEvtFuc() {
+        window.addEventListener("touchstart",handleTouchStart)
+        window.addEventListener("touchmove",handleTouchMove)
+        window.addEventListener("touchend",handleTouchEnd)
+    }
+    window.requestAnimationFrame(function step() {
+        const isScrollInside = judgeScrollInside(removeEvtFuc)
+        if(isScrollInside) return
+
+        window.requestAnimationFrame(step)
+    })
+}
+
 function handleTouchStart(evt: TouchEvent) {
     txs = evt.touches[0].clientX
     tys = evt.touches[0].clientY
-    if( teact_part!.getBoundingClientRect().top <= headerHeightMargin 
-        && teact_part!.getBoundingClientRect().bottom > 0
-        && !isInTeach
-    ) {
-        const scrollSection = document.getElementById('mainContent_scrollSection_ID')
-        scrollSection!.style.overflow = "hidden"
-        teact_part!.style.overflowX = "auto"
-        isInTeach = true
-        scrollSection!.removeEventListener("touchstart",handleTouchStart)
-        scrollSection!.removeEventListener("touchmove",detectInTeach)
-        window.addEventListener("touchstart",inTeachStart)
-        window.addEventListener("touchmove",handleTouchMove)
-        window.addEventListener("touchend",handleTouchEnd)
-    }
 }
 
-function inTeachStart(evt: TouchEvent) {
-    txs = evt.touches[0].clientX
-    tys = evt.touches[0].clientY
-}
-
-function detectInTeach() {
-    if( teact_part!.getBoundingClientRect().top <= headerHeightMargin 
-        && teact_part!.getBoundingClientRect().bottom > 0
-        && !isInTeach
-    ) {
-        const scrollSection = document.getElementById('mainContent_scrollSection_ID')
-        scrollSection!.style.overflow = "hidden"
-        teact_part!.style.overflowX = "auto"
-        isInTeach = true
-        scrollSection!.removeEventListener("touchstart",handleTouchStart)
-        scrollSection!.removeEventListener("touchmove",detectInTeach)
-        window.addEventListener("touchstart",inTeachStart)
-        window.addEventListener("touchmove",handleTouchMove)
-        window.addEventListener("touchend",handleTouchEnd)
-    }
+function removeTouchEvtFuc(isMoveForward: boolean) {
+    window.removeEventListener("touchmove",handleTouchMove)
+    window.removeEventListener("touchstart",handleTouchStart)
+    if(!isMoveForward) {
+        detectMobileScroll()
+    } 
 }
 
 function handleTouchMove(evt: TouchEvent) {
@@ -98,32 +107,9 @@ function handleTouchMove(evt: TouchEvent) {
     const scollSpeed = Math.abs(tye - tys)
 
     if(tye < tys || txe < txs) { // 往下滑(手指往上)或往右滑(手指往左)
-        teact_part!.scrollLeft = teact_part!.scrollLeft + scollSpeed
-        console.log(teact_part!.scrollLeft,teact_part!.scrollWidth - teact_part!.clientWidth - 3)
-        if(teact_part!.scrollLeft >= (teact_part!.scrollWidth - teact_part!.clientWidth - 3) ) {
-            const newScroll = scrollSection!.scrollTop + teact_part!.getBoundingClientRect().bottom
-            scrollSection!.style.overflow = ""
-            smoothScroll(newScroll,"mainContent_scrollSection_ID",1)
-            isInTeach = false
-            teact_part!.style.overflowX = "hidden"
-            teact_part!.scrollLeft = 0
-            window.removeEventListener("touchmove",handleTouchMove)
-            window.removeEventListener("touchstart",inTeachStart)
-            scrollSection!.removeEventListener("touchstart",handleTouchStart)
-        }
+        judgeMoveDirection(true,removeTouchEvtFuc,scollSpeed)
     } else if(tye > tys || txe > txs) { // 往上滑(手指往下)或往左滑(手指往右)
-        teact_part!.scrollLeft = teact_part!.scrollLeft - scollSpeed
-        if(teact_part!.scrollLeft <= 0) {
-            teact_part!.style.overflowX = "hidden"
-            teact_part!.scrollLeft = 0
-            const newScroll = scrollSection!.scrollTop - teact_part!.getBoundingClientRect().top
-            smoothScroll(newScroll,"mainContent_scrollSection_ID",2)
-            scrollSection!.style.overflow = ""
-            isInTeach = false
-            window.removeEventListener("touchmove",handleTouchMove)
-            window.removeEventListener("touchstart",inTeachStart)
-            scrollSection!.addEventListener("touchstart",handleTouchStart)
-        }
+        judgeMoveDirection(false,removeTouchEvtFuc,scollSpeed)
     }
 
     txs = txe, tys = tye
@@ -143,8 +129,7 @@ export function initTeachPart() {
     const {isMobile} = use_deviceInfo_Store()
 
     if(isMobile) {
-        scrollSection!.addEventListener("touchstart",handleTouchStart)
-        scrollSection!.addEventListener("touchmove",detectInTeach)
+        detectMobileScroll()
     } else {
         scrollSection!.addEventListener("scroll",handleScroll)
         window.addEventListener("wheel",handleWheel)
